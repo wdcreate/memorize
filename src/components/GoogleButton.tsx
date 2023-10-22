d6e630c03979f09a50320fd49dc311d386038cdd
Vlad <73606799+wdcreate@users.noreply.github.com>
2023-10-22 12:54:46 +0300

    some small fix

--- src/components/GoogleButton.tsx
+++ src/components/GoogleButton.tsx
@@ -10,30 +10,21 @@ const GoogleButton = ({ sizeSmall }: gbType) => {
   const { googleAuth } = UserAuth();
 
   return (
-    <div>
-      {sizeSmall ? (
-        <div className="google-btn-spec" onClick={() => googleAuth()}>
-          <div className="google-icon-spec">
-            <img
-              className="google-icon"
-              alt="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-            />
-          </div>
-        </div>
-      ) : (
-        <div className="google-btn" onClick={() => googleAuth()}>
-          <div className="google-icon-wrapper">
-            <img
-              className="google-icon"
-              alt="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-            />
-          </div>
-          <p className="btn-text">
-            <b>Continue with Google</b>
-          </p>
-        </div>
+    <div
+      className={sizeSmall ? "google-btn-spec" : "google-btn"}
+      onClick={() => googleAuth()}
+    >
+      <div className={sizeSmall ? "google-icon-spec" : "google-icon-wrapper"}>
+        <img
+          className="google-icon"
+          alt="logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+        />
+      </div>
+      {!sizeSmall && (
+        <p className="btn-text">
+          <b>Continue with Google</b>
+        </p>
       )}
     </div>
   );
